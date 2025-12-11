44bb188842077e31e8aa180fe9f70e2d6e3b1de7
Erik Weilandt <48921674+inctechs@users.noreply.github.com>
2025-12-11 13:57:28 +0100

    fix lsp set up issue
    Mason 2.0 does not use handlers anymore also clang needs certain flags
    to work properly

--- init.lua
+++ init.lua
@@ -730,7 +730,24 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          cmd = {
+            'clangd',
+            '--background-index',
+            '--clang-tidy',
+            '--header-insertion=iwyu',
+            '--completion-style=detailed',
+            '--function-arg-placeholders',
+            '--fallback-style=llvm',
+            '--enable-config',
+            -- '--query-driver=/usr/bin/clang++,/usr/bin/g++,/usr/bin/*,/Library/Developer/CommandLineTools/usr/bin/*',
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
         -- gopls = {},
         pyright = {
           settings = {
@@ -783,23 +800,6 @@ require('lazy').setup({
         },
       }
 
-      -- Ensure that pyright is not used for diagnostics as linint but instead ruff is used
-      -- Configure pyright without diagnostics
-      -- require('lspconfig').pyright.setup {
-      --   settings = {
-      --     pyright = {
-      --       -- Using Ruff's import organizer
-      --       disableOrganizeImports = true,
-      --     },
-      --     python = {
-      --       analysis = {
-      --         -- Ignore all files for analysis to exclusively use Ruff for linting
-      --         ignore = { '*' },
-      --       },
-      --     },
-      --   },
-      -- }
-
       -- Ensure the servers and tools above are installed
       --
       -- To check the current status of installed tools and/or manually install
@@ -816,24 +816,34 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        -- 'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+      -- The following loop will configure each server with the capabilities we defined above.
+      -- This will ensure that all servers have the same base configuration, but also
+      -- allow for server-specific overrides.
+      for server_name, server_config in pairs(servers) do
+        server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
+        vim.lsp.config(server_name, server_config)
+        vim.lsp.enable(server_name)
+      end
+
+      -- Ensure that pyright is not used for diagnostics as linint but instead ruff is used
+      -- Configure pyright without diagnostics
+      -- require('lspconfig').pyright.setup {
+      --   settings = {
+      --     pyright = {
+      --       -- Using Ruff's import organizer
+      --       disableOrganizeImports = true,
+      --     },
+      --     python = {
+      --       analysis = {
+      --         -- Ignore all files for analysis to exclusively use Ruff for linting
+      --         ignore = { '*' },
+      --       },
+      --     },
+      --   },
+      -- }
     end,
   },
 
